18537fc5b368b9b0b03169bb224fee2eeb82410f
Xiamu-ssr <1138882663@qq.com>
2023-09-23 16:27:30 +0800

    add some Data Structure java file

---
+++ DataSt/src/main/java/org/kmo/List/ArrayListClass.java
@@ -0,0 +1,48 @@
+package org.kmo.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+public class ArrayListClass {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger("");
+        ArrayList<String> arr = new ArrayList<String>();
+//        add
+        logger.info("add");
+        arr.add("jack");
+        arr.add("naco");
+        arr.add("naco");
+        arr.add("need");
+        System.out.println(arr);
+//        addAll
+        logger.info("addAll");
+        ArrayList<String> arr2 = new ArrayList<String>();
+        arr2.addAll(0,arr);
+        System.out.println(arr2);
+//        clear
+        logger.info("clear");
+        arr2.clear();
+        System.out.println(arr2);
+//        contains
+        logger.info("contains");
+        System.out.println(arr.contains("need"));
+//        get
+        logger.info("get");
+        System.out.println(arr.get(1));
+//        indexOf
+        logger.info("indexOf");
+        System.out.println(arr.indexOf("need"));
+//        removeAll
+        logger.info("removeAll");
+        arr2.add(arr.get(1));
+        arr.removeAll(arr2);
+        System.out.println(arr);
+//        size
+        logger.info("size");
+        System.out.println(arr.size());
+//        更多api请看https://www.runoob.com/manual/jdk11api/java.base/java/util/ArrayList.html
+    }
+}
